2ebf443d9dd148e257fbdf4dd5aedce2844130e2
Hussnain Younas <findhussnain@gmail.com>
2021-12-19 00:41:02 +0500

    Create User, Message resources and add relationship between them

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Hussnain", password: "hussnain")
+User.create(username: "User1", password: "user1")
+User.create(username: "User2", password: "user2")
+User.create(username: "User3", password: "user3")
+User.create(username: "User4", password: "user4")
